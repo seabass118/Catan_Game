158a6c5b333a699d5209f79cf815f9f96d601d17
FIXED-TERM Blackley Sebastian (AA/EOP2-PE) <bls1mnc@bosch.com>
2023-08-10 16:00:11 +0100

    created map + added node state

--- src/app.jsx
+++ src/app.jsx
@@ -1,34 +1,81 @@
 import { useState } from "preact/hooks";
 
 const map = [
-  { row: [0, 1, 2] },
-  { row: [3, 4, 5, 6] },
-  { row: [7, 8, 9, 10, 11] },
-  { row: [12, 13, 14, 15] },
-  { row: [16, 17, 18] },
+    {
+        row: [
+            { id: 0 },
+            { id: 1 },
+            { id: 2 }
+        ],
+    },
+    { 
+        row: [
+            { id: 3 },
+            { id: 4 },
+            { id: 5 },
+            { id: 6 }
+        ] 
+    },
+    { 
+        row: [
+            { id: 7 },
+            { id: 8 },
+            { id: 9 },
+            { id: 10 },
+            { id: 11 }
+        ] 
+    },
+    { 
+        row: [
+            { id: 12 },
+            { id: 13 },
+            { id: 14 },
+            { id: 15 }
+        ] 
+    },
+    { 
+        row: [
+            { id: 16 },
+            { id: 17 },
+            { id: 18 },
+        ] 
+    },
 ];
 
 export function App() {
-  const [selectedNode, setSelectedNode] = useState(null);
+    const [selectedNode, setSelectedNode] = useState([]);
 
-  return (
-    <div className="w-full h-screen flex-col items-center justify-center flex">
-      {map.map((obj, id) => {
-        return (
-          <div className="flex flex-row">
-            {obj.row.map((node) => {
-              return (
-                <div
-                  //Set state for selected node
-                  className="bg-emerald-400 w-[100px] h-[100px] font-bold mask mask-hexagon text-white flex items-center justify-center hover:bg-black cursor-pointer"
-                >
-                  {node}
-                </div>
-              );
+    return (
+        <div>
+            {selectedNode.map((i) => {
+                return (
+                    <div className=" absolute font-bold text-2xl p-6">
+                        Node ID: {i.id} <br></br> Row: {i.row + 1} <br></br> Position: {i.position + 1}
+                    </div>
+                    
+                )
             })}
-          </div>
-        );
-      })}
-    </div>
-  );
+            <div className="w-full h-screen flex-col items-center justify-center flex">
+            {map.map((obj, id) => {
+                return (
+                    <div className="flex flex-row">
+                        {obj.row.map((node, index) => {
+                            return (
+                                <div
+                                    //Display node state data
+                                    onMouseOver={() => setSelectedNode([{id: node.id, position: index, row: id}])}
+                                    onMouseOut={() => setSelectedNode([])}
+                                    className="bg-emerald-400 w-[100px] h-[100px] font-bold mask mask-hexagon text-white flex items-center justify-center hover:opacity-50 cursor-pointer"
+                                >
+                                    {node.id}
+                                </div>
+                            );
+                        })}
+                    </div>
+                );
+            })}
+        </div>
+        </div>
+        
+    );
 }
